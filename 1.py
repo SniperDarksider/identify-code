cb75e673a7a1481a647c5f76ece516855412be97
SniperDarksider <1264191896@qq.com>
2017-06-24 16:53:47 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,38 @@
+import numpy as np
+import argparse
+import cv2
+
+ap=argparse.ArgumentParser()
+ap.add_argument("-i","--image",required=True,help="path to the image")
+args=vars(ap.parse_args())
+
+image=cv2.imread(args["image"])
+gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+gradX=cv2.Sobel(gray,ddepth=cv2.CV_32F,dx=1,dy=0,ksize=-1)
+gradY=cv2.Sobel(gray,ddepth=cv2.CV_32F,dx=0,dy=1,ksize=-1)
+
+gradient=cv2.subtract(gradX,gradY)
+gradient=cv2.convertScaleAbs(gradient)
+
+blurred=cv2.blur(gradient,(9,9))
+(_,thresh)=cv2.threshold(blurred,225,225,cv2.THRESH_BINARY)
+
+kernel=cv2.getStructuringElement(cv2.MORPH_RECT,(21,7))
+closed=cv2.morphologyEx(thresh,cv2.MORPH_CLOSE,kernel)
+
+closed=cv2.erode(closed,None,iterations=4)
+closed=cv2.dilate(closed,None,iterations=4)
+
+
+closed_1=closed.copy()
+_,cnts,_=cv2.findContours(closed_1,0,2)
+c=sorted(cnts,key=cv2.contourArea,reverse=True)[0]
+
+rect=cv2.minAreaRect(c)
+box=np.int0(cv2.boxPoints(rect))
+
+cv2.drawContours(image,[box],-1,(0,255,0),3)
+cv2.imshow("Image",image)
+cv2.waitKey(0)
+
